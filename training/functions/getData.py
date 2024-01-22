b1fad82ebb31caabdcdf29541913143f499187c0
Ashu11-A <Matheusn.biolowons@gmail.com>
2024-01-22 17:09:30 -0400

    📦 Create directory

--- training/functions/getData.py
+++ training/functions/getData.py
@@ -52,7 +52,6 @@ class DataLoader:
                 if entry.is_dir(follow_symlinks=False):
                     scan_directory(entry.path)
                 elif entry.is_file(follow_symlinks=False) and entry.path.endswith(('.png', '.jpg', '.jpeg', '.webp')):
-                    # filename, file_extension = os.path.splitext(entry.path)
                     mark_path = Path(entry.path.replace("train", "validation")).with_suffix('.png')
                     if mark_path.exists():
                         images.append(entry.path)
@@ -69,6 +68,8 @@ class DataLoader:
 
     def countFolders(self, directory: str) -> int:
         count = 0
+        if not os.path.exists(directory):
+            os.mkdir(directory)
         for entry in os.scandir(directory):
             if entry.is_dir(follow_symlinks=False):
                 count += 1
