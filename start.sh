11a85a173f3f56333671c370c7eade3b790024c6
Ashu11-A <Matheusn.biolowons@gmail.com>
2024-01-22 17:08:12 -0400

    💹 Modify the start script

--- start.sh
+++ start.sh
@@ -1,10 +1,24 @@
-ARCH=$(uname -m)
+#!/bin/bash
 
-source "/home/container/.nvm/nvm.sh"
-export NVM_DIR=/home/container/.nvm
+export ARCH=$(uname -m)
 export LD_PRELOAD=/usr/lib/$ARCH-linux-gnu/libjemalloc.so.2
-nvm install 21
-nvm use 21
+if [ ! -z "$(command -v nvm)" ]; then
+    source "$HOME/.nvm/nvm.sh"
+    export NVM_DIR="$HOME/.nvm"
+    echo "Usando o NVM"
+    nvm install 21
+    nvm use 21
+elif [ ! -z "$(command -v fnm)" ]; then
+    export PATH="$HOME/.local/share/fnm:$PATH"
+    echo "Usando o FNM"
+    fnm install 21
+    fnm use 21
+elif [[ "$(node -v)" == v21.* ]]; then
+    echo "Usando o Nodejs $(node -v)"
+fi
+node --version
 npm install
-npm rebuild @tensorflow/tfjs-node --build-from-source
+if [ "$ARCH" == "aarch64"]; then
+    npm rebuild @tensorflow/tfjs-node --build-from-source
+fi
 npm run production
